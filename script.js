b1ec445501f79362873e14e4b39739230ce99764
Vishal Patel <vishal@vishal.rs>
2024-02-16 18:59:43 -0800

    Now sensing changes to lists

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Trello List Counts
 // @namespace    https://trello.com/
-// @version      1.0
+// @version      1.1
 // @description  Add list counts to trello lists.
 // @author       Vishal Patel
 // @match        https://trello.com/b/*
@@ -10,17 +10,35 @@
 (function() {
     'use strict';
 
-    function addCounts() {
+    function updateCounts() {
         document.querySelectorAll('[data-testid="list"]').forEach((t_list) => {
             let list_count = t_list.querySelectorAll('li').length;
-            let list_name = t_list.querySelector('h2').innerText;
+            let list_name = t_list.querySelector('[data-testid="list-name"]').nextSibling.value;
             t_list.querySelector('h2').innerText = list_name+" ("+list_count+")";
         });
     }
 
-    window.setTimeout(function() {
-        addCounts();
-        //showCardNumbers();
-    },2000);
+    function observeLists() {
+        let observer = new MutationObserver(mutations => {
+            for(let mutation of mutations) {
+                // examine new nodes, is there anything to highlight?
+
+                for(let node of mutation.addedNodes) {
+                    // we track only elements, skip other nodes (e.g. text nodes)
+                    if (!(node instanceof HTMLElement)) continue;
+                    updateCounts()
+                }
+            }
+        });
+
+        document.querySelectorAll('[data-testid="list"]').forEach((t_list) => {
+            observer.observe(t_list, {childList: true, subtree: true});
+        });
+    }
+
+    window.onload = () => {
+        updateCounts();
+        observeLists();
+    }
 
 })();
